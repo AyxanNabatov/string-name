92007630bb1ed1e225eb1ac411d7aaf2d07a19fb
AyxanNabatov <107032159+AyxanNabatov@users.noreply.github.com>
2022-06-21 20:10:50 -0700

    Add files via upload

---
+++ string/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Melek";
+
+            string name2 = name1;
+
+            name2 = "Kelem";
+
+            Console.WriteLine(name2);
+
+        }
+    }
+}
